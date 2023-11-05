ebe7e0335553255e25df168808b0fdee5cdc4d80
Ruslan <teslajke88@gmail.com>
2023-11-05 20:44:59 +0300

    change color; add pricing; change testimonials

--- assets/js/counter.js
+++ assets/js/counter.js
@@ -3,12 +3,21 @@ const currentDateMilliseconds = new Date().getTime();
 let currentDateMillisecondsToDays =
   currentDateMilliseconds / (1000 * 60 * 60 * 24);
 
+const purecounterElements = document.querySelectorAll(".purecounter");
+
+for (let elem of purecounterElements) {
+  elem.style.color = "black";
+}
+
+let dealsDone = currentDateMillisecondsToDays - endNumber;
+
 document
   .querySelector(".purecounter1")
-  .setAttribute(
-    "data-purecounter-end",
-    currentDateMillisecondsToDays - endNumber
-  );
+  .setAttribute("data-purecounter-end", dealsDone);
+
+document
+  .querySelector(".purecounter2")
+  .setAttribute("data-purecounter-end", dealsDone * 135000);
 
 console.log(currentDateMilliseconds);
 console.log(currentDateMillisecondsToDays);
